8147a43ae910f301311ffa4b298c5583ea364cdb
Linus Foxell <linusfoxell@hotmail.com>
2022-06-23 03:11:59 +0200

    Add socket endpoint +  data recieve

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
+import { useEffect, useState, useRef } from "react";
 import JoinRoom from "./pages/JoinRoom";
+import { io } from "socket.io-client";
+
+let socket = io("http://localhost:4000");
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+  const [users, setUsers] = useState([]);
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to server");
+      setCurrentUser({ ...currentUser, id: socket.id });
+      socket.emit("ready");
+    });
+
+    socket.on("initial_data", (data) => {
+      setRooms(data.rooms);
+      setUsers(data.users);
+    });
+
+    return () => socket.off();
+  });
+
+  const handleClick = () => {
+    console.log(rooms);
+    console.log(users);
+    console.log(currentUser);
+  };
+
   return (
     <div className="App">
-      <JoinRoom />
+      <JoinRoom rooms={rooms} />
+      <div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={handleClick}
+        >
+          Button
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/JoinRoom.jsx
+++ src/pages/JoinRoom.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { io } from "socket.io-client";
 
-const JoinRoom = () => {
+let socket = io("http://localhost:4000");
+
+const JoinRoom = ({ rooms }) => {
   return (
     <>
       <div className="flex justify-center items-center min-h-screen">
@@ -25,11 +28,9 @@ const JoinRoom = () => {
             </label>
             <div className="inline-block relative w-64">
               <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                <option>Room 1</option>
-                <option>Room 2</option>
-                <option>Room 3</option>
-                <option>Room 4</option>
-                <option>Room 5</option>
+                {rooms.map((room) => {
+                  return <option>{room.name}</option>;
+                })}
               </select>
               <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                 <svg
@@ -45,6 +46,9 @@ const JoinRoom = () => {
               <button
                 className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-5"
                 type="button"
+                onClick={() => {
+                  console.log(rooms);
+                }}
               >
                 Join room
               </button>
