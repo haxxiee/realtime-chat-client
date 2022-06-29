905c8472cfa34953488520f6c8c38db8de25302c
Linus Foxell <linusfoxell@hotmail.com>
2022-06-29 05:06:29 +0200

    add design func + message data

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ let socket = io("http://localhost:4000");
 
 function App() {
   const [username, setUsername] = useState("");
+  const [addRoom, setAddRoom] = useState("");
   const [currentUser, setCurrentUser] = useState({});
+  const [messages, setMessages] = useState([]);
   const [users, setUsers] = useState([]);
   const [rooms, setRooms] = useState([]);
   const [currentRoom, setCurrentRoom] = useState();
@@ -29,24 +31,42 @@ function App() {
     });
 
     socket.on("user_info", (data) => {
-      console.log(data);
       setCurrentUser(data);
+    });
+
+    socket.on("messages", (data) => {
+      setMessages(data);
     });
 
     return () => socket.off();
   });
 
+  useEffect(() => {
+    console.log("nu ändrras de", currentRoom);
+    // Emitar vart man ska vara connectad
+    socket.emit("get_messages", currentRoom);
+  }, [currentRoom]);
+
   const handleClick = () => {
     console.log(rooms);
     console.log(users);
-    socket.emit("create_room", "TEST");
+    console.log(addRoom);
+
+    socket.emit("create_room", addRoom);
     socket.emit("ready");
+    setAddRoom("");
   };
 
   const handleUserClick = () => {
     setCurrentUser({ name: username });
-    console.log(username);
     socket.emit("create_user", username);
+    socket.emit("ready");
+  };
+
+  const handleDeleteClick = () => {
+    console.log(currentRoom);
+    socket.emit("delete_room", currentRoom);
+    setCurrentRoom(undefined);
     socket.emit("ready");
   };
 
@@ -122,13 +142,106 @@ function App() {
         </>
       ) : null}
 
-      <div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={handleClick}
-        >
-          Button
-        </button>
+      <div className="flex justify-between items-center mt-16">
+        {currentUser.name ? (
+          <div className="flex justify-center items-center ml-20">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 mr-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="room"
+              type="text"
+              placeholder="Enter Room Name"
+              value={addRoom}
+              onChange={(e) => setAddRoom(e.target.value)}
+            />
+            <button
+              className="bg-blue-500 hover:blue-red-700 text-white font-bold py-2 px-3  rounded-md w-full"
+              onClick={handleClick}
+            >
+              Add Room
+            </button>
+          </div>
+        ) : null}
+        {currentUser.name && currentRoom !== undefined ? (
+          <div className="mr-20">
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md"
+              onClick={handleDeleteClick}
+            >
+              Delete this room
+            </button>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+
+      <div className="flex justify-center items-center ">
+        <div className="flex justify-start items-center w-96 h-96 overflow-auto ">
+          <ul>
+            <div className="my-3">
+              <h2 className="font-bold text-blue-600">Bosse</h2>
+              <p>Tjenare mannen!</p>
+            </div>
+            <div className="my-3">
+              <h2 className="font-bold text-blue-600">Lasse</h2>
+              <p>Hej på dig!</p>
+            </div>
+            <div className="my-3">
+              <h2 className="font-bold text-blue-600">Lasse</h2>
+              <p>Jo men det är bra med mig, sj?</p>
+            </div>
+            <div className="my-3">
+              <h2 className="font-bold text-blue-600">Lasse</h2>
+              <p>Hej på dig!</p>
+            </div>
+            <div className="my-3">
+              <h2 className="font-bold text-blue-600">Lasse</h2>
+              <p>Hej på dig!</p>
+            </div>
+            <div className="my-3">
+              <h2 className="font-bold text-blue-600">Lasse</h2>
+              <p>Hej på dig!</p>
+            </div>
+            <div className="my-3">
+              <h2 className="font-bold text-blue-600">Lasse</h2>
+              <p>Hej på dig!</p>
+            </div>
+            <div className="my-3">
+              <h2 className="font-bold text-blue-600">Lasse</h2>
+              <p>Hej på dig!</p>
+            </div>
+            <div className="my-3">
+              <h2 className="font-bold text-blue-600">Lasse</h2>
+              <p>Hej på dig!</p>
+            </div>
+          </ul>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <form>
+          <div class="mb-4 w-96 bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+            <div class="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
+              <label for="comment" class="sr-only">
+                Your comment
+              </label>
+              <textarea
+                id="comment"
+                rows="4"
+                class="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                placeholder="Write a comment..."
+                required
+              ></textarea>
+            </div>
+            <div class="flex justify-between items-center py-2 px-3 border-t dark:border-gray-600">
+              <button
+                type="submit"
+                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+              >
+                Post comment
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
